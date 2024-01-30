f641f105c91af35183d65ca313fba97f4e1538cd
wfraga <walterfraga@yandex.com>
2024-01-30 13:01:22 -0500

    Added random word selection

--- Wordle.py
+++ Wordle.py
@@ -1,1 +1,12 @@
-print("Hi Wordle")
+import random
+
+
+def main():
+    word_file = open("words.txt", "r")
+    words = word_file.readlines()
+    chosen_word = random.choice(words)
+    print(chosen_word)
+
+
+if __name__ == "__main__":
+    main()
