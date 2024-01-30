d27875234a32e5fbd3aebc168c758c596a4f2eb6
wfraga <walterfraga@yandex.com>
2024-01-30 15:40:41 -0500

    Added primitive guessing

--- Wordle.py
+++ Wordle.py
@@ -5,7 +5,13 @@ def main():
     word_file = open("words.txt", "r")
     words = word_file.readlines()
     chosen_word = random.choice(words)
-    print(chosen_word)
+
+    for index in range(6):
+        input_word = input('Gues my word?\n')
+        if input_word == chosen_word:
+            print("Yeah you guessed my word")
+            exit(0)
+    print('You were unable to guess my word\n. It was: ' + chosen_word)
 
 
 if __name__ == "__main__":
