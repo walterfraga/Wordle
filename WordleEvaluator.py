b02e616d5961acddbaaad14aba4fd8dfe2c5d76b
wfraga <walterfraga@yandex.com>
2024-01-30 22:00:26 -0500

    primitive evaluator

--- WordleEvaluator.py
+++ WordleEvaluator.py
@@ -5,7 +5,14 @@ class WordleEvaluator:
 
     def evaluate(self, chosen_word, guess_word):
         hint = ''
+        index = 0
         for letter in guess_word:
             if letter in chosen_word:
-                hint += letter
+                if chosen_word[index] == letter:
+                    hint += letter + ' - Correct Place\n'
+                else:
+                    hint += letter + ' - In word\n'
+            else:
+                hint += letter + ' - Not in Word\n'
+            index = index + 1
         return hint
