57798bcde865851fe38bdd72df6312329dabed64
wfraga <walterfraga@yandex.com>
2024-02-01 08:01:32 -0500

    Completed TestWordleValidator class

--- src/game/validator/WordleValidaor.py
+++ src/game/validator/WordleValidaor.py
@@ -8,10 +8,11 @@ class WordleValidator:
 
     def validate(self, word):
         reasons = []
-        if not len(word) == 5:
-            reasons.append(InvalidWordReason.INVALID_LENGTH)
-        if not word.isalpha():
-            reasons.append(InvalidWordReason.NOT_ALPHA)
-        if not len(reasons) == 0:
-            return reasons
+        if word is not None:
+            if not len(word) == 5:
+                reasons.append(InvalidWordReason.INVALID_LENGTH)
+            if not word.isalpha():
+                reasons.append(InvalidWordReason.NOT_ALPHA)
+            if not len(reasons) == 0:
+                return reasons
         return None
--- test/test_WordleValidaor.py
+++ test/test_WordleValidaor.py
@@ -1,13 +1,43 @@
-from unittest import TestCase
-
+import unittest
 from game.validator.InvalidWordReason import InvalidWordReason
 from game.validator.WordleValidaor import WordleValidator
 
 
-class TestWordleValidator(TestCase):
+class TestWordleValidator(unittest.TestCase):
+
+    def test_should_return_none_when_word_is_none(self):
+        word = None
+        wordle_validator = WordleValidator()
+        result = wordle_validator.validate(word)
+        self.assertEqual(None, result)
+
+    def test_should_return_invalid_length_when_word_is_empty(self):
+        word = ''
+        wordle_validator = WordleValidator()
+        result = wordle_validator.validate(word)
+        self.assertEqual(InvalidWordReason.INVALID_LENGTH.value, result[0].value)
+
+    def test_should_return_invalid_length_when_word_is_shorted_than_five_and_alpha(self):
+        word = 'qa'
+        wordle_validator = WordleValidator()
+        result = wordle_validator.validate(word)
+        self.assertEqual(InvalidWordReason.INVALID_LENGTH.value, result[0].value)
+
     def test_should_return_non_alpha(self):
         word = '12345'
         wordle_validator = WordleValidator()
         result = wordle_validator.validate(word)
+        self.assertEqual(1, len(result))
         self.assertEqual(InvalidWordReason.NOT_ALPHA.value, result[0].value)
 
+    def test_should_return_non_alpha_and_invalid_length_when_word_in_non_numerci_and_shorter_than_5(self):
+        word = '1234'
+        wordle_validator = WordleValidator()
+        result = wordle_validator.validate(word)
+        self.assertEqual(2, len(result))
+        self.assertEqual(InvalidWordReason.INVALID_LENGTH.value, result[0].value)
+        self.assertEqual(InvalidWordReason.NOT_ALPHA.value, result[1].value)
+
+
+if __name__ == '__main__':
+    unittest.main()
