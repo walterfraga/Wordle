2b1cf8f6d9714ee92703d383db23263ce1c043d7
wfraga <walterfraga@yandex.com>
2024-02-01 20:47:39 -0500

    completed missing tests in test_WordleGame.pyWordleGame

--- test/test_WordleGame.py
+++ test/test_WordleGame.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from unittest.mock import Mock, MagicMock
 
 from game.WordleGame import WordleGame
+from game.evaluator.Hint import Hint
 from game.model.Guess import Guess
 from game.validator.InvalidWordReason import InvalidWordReason
 from game.validator.WordleValidaor import WordleValidator
@@ -9,7 +10,7 @@ from game.validator.WordleValidaor import WordleValidator
 
 class TestWordleGame(TestCase):
 
-    def test_is_chosen_word(self):
+    def test_should_identify_chosen_word_as_invalid(self):
         # given
         guess_word = 'words'
         invalid_reasons = [InvalidWordReason.INVALID_LENGTH]
@@ -26,3 +27,58 @@ class TestWordleGame(TestCase):
         # then
         self.assertEqual(Guess.INVALID.value, result.guess.value)
         self.assertEqual(invalid_reasons, result.invalid_reasons)
+        self.assertEqual(None, result.hints)
+
+    def test_should_identify_chosen_word_as_expired(self):
+        # given
+        guess_word = 'words'
+        validator = WordleValidator()
+        validator.validate = MagicMock(return_value=None)
+        evaluator = Mock()
+        word_picker = Mock()
+        wordle_game = WordleGame(0, word_picker, validator, evaluator)
+
+        # when
+        result = wordle_game.is_chosen_word(guess_word)
+
+        # then
+        self.assertEqual(Guess.EXPIRED.value, result.guess.value)
+        self.assertEqual(None, result.invalid_reasons)
+        self.assertEqual(None, result.hints)
+
+    def test_should_identify_chosen_word_as_correct(self):
+        # given
+        guess_word = 'words'
+        validator = WordleValidator()
+        validator.validate = MagicMock(return_value=None)
+        evaluator = Mock()
+        word_picker = Mock()
+        word_picker.pick_word = MagicMock(return_value=guess_word)
+        wordle_game = WordleGame(1, word_picker, validator, evaluator)
+
+        # when
+        result = wordle_game.is_chosen_word(guess_word)
+
+        # then
+        self.assertEqual(Guess.CORRECT.value, result.guess.value)
+        self.assertEqual(None, result.invalid_reasons)
+        self.assertEqual(None, result.hints)
+
+    def test_should_identify_chosen_word_as_incorrect(self):
+        # given
+        guess_word = 'words'
+        validator = WordleValidator()
+        validator.validate = MagicMock(return_value=None)
+        evaluator = Mock()
+        hints = [dict(letter='', hint=Hint.CORRECT_PLACE)]
+        evaluator.evaluate = MagicMock(return_value=hints)
+        word_picker = Mock()
+        wordle_game = WordleGame(1, word_picker, validator, evaluator)
+
+        # when
+        result = wordle_game.is_chosen_word(guess_word)
+
+        # then
+        self.assertEqual(Guess.INCORRECT.value, result.guess.value)
+        self.assertEqual(None, result.invalid_reasons)
+        self.assertEqual(hints, result.hints)
