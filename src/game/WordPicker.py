edff311b703aed28c9ac3ce5cd1f8c6786d182b7
wfraga <walterfraga@yandex.com>
2024-02-03 11:51:41 -0500

    Added relative path to the words file

--- src/game/WordPicker.py
+++ src/game/WordPicker.py
@@ -3,6 +3,6 @@ import random
 
 class WordPicker:
     def pick_word(self):
-        word_file = open("src/game/resources/words.txt", "r")
+        word_file = open("../src/game/resources/words.txt", "r")
         words = word_file.readlines()
         return random.choice(words)
