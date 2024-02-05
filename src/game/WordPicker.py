636f38550b66bca03fe91ada1891954efaffe949
wfraga <walterfraga@yandex.com>
2024-02-05 08:50:42 -0500

    Adjusted words.txt folder search

--- src/game/WordPicker.py
+++ src/game/WordPicker.py
@@ -1,8 +1,10 @@
 import random
+import os
 
 
 class WordPicker:
     def pick_word(self):
-        word_file = open("../src/game/resources/words.txt", "r")
+        working_directory = os.getcwd()
+        word_file = open(working_directory + "/src/game/resources/words.txt", "r")
         words = word_file.readlines()
         return random.choice(words)
