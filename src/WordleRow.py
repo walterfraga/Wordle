3140592a3786fc2dee53883b01a16b85abcab0df
wfraga <walterfraga@yandex.com>
2024-02-07 09:53:37 -0500

    - First draft of working rows - Displaying remaining letter - Displaying invalid words

--- src/WordleRow.py
+++ src/WordleRow.py
@@ -1,16 +1,13 @@
 import tkinter as tk
 
 
-
-
-
 class WordleRow:
     # Ensured one entry in text
     def validate_one_entry(self, text):
         return not len(text) > 1
 
     def get_letter(self, entry):
-        for child in self.root.grid_slaves():
+        for child in self.wordle_ui.root.grid_slaves():
             if child == entry:
                 return child.get()
 
@@ -20,7 +17,6 @@ class WordleRow:
         # travers in reverse
         for entry in self.entries:
             word += self.get_letter(entry)
-            print(word)
             child_column_position = entry.grid_info()['column']
             if child_column_position == current_column_position + 1:
                 if str(event.char).isalnum():
@@ -30,7 +26,8 @@ class WordleRow:
                     entry.config(state='disabled')
 
         if len(word) == 5 and (event.keysym == 'KP_Enter' or event.keysym == 'Return'):
-            self.entered_word = word
+            self.disable_rows()
+            self.wordle_ui.callback(word)
         return
 
     def get_current_position(self, event):
@@ -41,20 +38,26 @@ class WordleRow:
                 break
         return current_column_position
 
-    def __init__(self, root, row, num_entries):
-        self.root = root
-        self.entered_word = ''
+    def disable_rows(self):
+        for child in self.entries:
+            child.config(state='disabled')
+
+    def __init__(self, wordle_ui, row, num_entries):
+        self.wordle_ui = wordle_ui
         self.entries = []
 
-        validation = root.register(self.validate_one_entry)
+        validation = self.wordle_ui.root.register(self.validate_one_entry)
         for col in range(num_entries):
-            root.columnconfigure(row, weight=1)
-            entry = tk.Entry(root, width=5, validate="key", validatecommand=(validation, "%P"))
+            self.wordle_ui.root.columnconfigure(row, weight=1)
+            entry = tk.Entry(self.wordle_ui.root, width=5, validate="key", validatecommand=(validation, "%P"))
             entry.grid(row=row, column=col, pady='5')
             entry.bind("<KeyPress>", lambda event: self.key_press_event(event, entry))
+            entry.config(state='disabled')
             self.entries.append(entry)
-            if col != 0:
-                entry.config(state='disabled')
+
+    def start(self):
+        self.entries[0].config(state='normal')
+        self.entries[0].focus_set()
 
     def get_values(self):
         return [entry.get() for entry in self.entries]
--- src/WordleUI.py
+++ src/WordleUI.py
@@ -1,26 +1,86 @@
 import tkinter as tk
 
 from WordleRow import WordleRow
-
+from game.WordleGame import WordleGame
+from game.evaluator.WordleEvaluator import WordleEvaluator
+from game.model.Guess import Guess
+from game.services.WordsService import WordsService
+from game.validator.WordleValidaor import WordleValidator
 
 class WordleUI:
+
+    def init_game(self):
+        number_of_guesses = 6
+        validator = WordleValidator()
+        evaluator = WordleEvaluator()
+        word_service = WordsService(5)
+        return WordleGame(number_of_guesses, word_service, validator, evaluator)
+
+    def enable_row(self, guesses):
+        if guesses == 5:
+            self.wordle_row2.start()
+        elif guesses == 4:
+            self.wordle_row3.start()
+        elif guesses == 3:
+            self.wordle_row4.start()
+        elif guesses == 2:
+            self.wordle_row5.start()
+        elif guesses == 1:
+            self.wordle_row6.start()
+
     def __init__(self):
-        root = tk.Tk()
-        root.title('Wordle')
-        root.geometry('250x250')
-        root['bg'] = '#AC99F2'
+        self.root = tk.Tk()
+        self.root.title('Wordle')
+        self.root.geometry('250x250')
+        self.root['bg'] = '#AC99F2'
 
-        label = tk.Label(root, text='Wordle')
+        self.wordle_game = self.init_game()
+        label = tk.Label(self.root, text='Wordle')
         label.grid(row=0, columnspan=5)
-        root.columnconfigure(0, weight=1)
+        self.root.columnconfigure(0, weight=1)
 
-        self.wordle_row1 = WordleRow(root, 1, 5)
-        #self.wordle_row2 = WordleRow(root, 2, 5)
-        #self.wordle_row3 = WordleRow(root, 3, 5)
-        #self.wordle_row4 = WordleRow(root, 4, 5)
-        #self.wordle_row5 = WordleRow(root, 5, 5)
-        #self.wordle_row6 = WordleRow(root, 6, 5)
-        root.mainloop()
+        self.wordle_row1 = WordleRow(self, 1, 5)
+        self.wordle_row2 = WordleRow(self, 2, 5)
+        self.wordle_row3 = WordleRow(self, 3, 5)
+        self.wordle_row4 = WordleRow(self, 4, 5)
+        self.wordle_row5 = WordleRow(self, 5, 5)
+        self.wordle_row6 = WordleRow(self, 6, 5)
+
+        self.status_label = tk.Label(self.root, text='')
+        self.status_label.grid(row=7, columnspan=5)
+
+        self.remaining_letters = tk.Label(self.root, text='')
+        self.remaining_letters.grid(row=8, columnspan=5)
+
+        self.callback()
+        self.root.mainloop()
+
+
+    def callback(self, word=None):
+        if word is None:
+            self.wordle_row1.start()
+            return
+        wordle_guess_result = self.wordle_game.is_chosen_word(word)
+        print(wordle_guess_result.guess)
+        if wordle_guess_result.guess.value == Guess.INVALID.value:
+            reasons = ''
+            for invalid_reason in wordle_guess_result.invalid_reasons:
+                reasons += invalid_reason.value
+            self.status_label.config(text=reasons)
+        if wordle_guess_result.guess.value == Guess.INCORRECT.value:
+            self.enable_row(self.wordle_game.guesses)
+            # self.display_hints(wordle_guess_result)
+            remaining_letter = ''
+            for letter in self.wordle_game.get_remaining_letters():
+                remaining_letter += letter
+            self.remaining_letters.config(text=remaining_letter)
+
+        if wordle_guess_result.guess.value == Guess.CORRECT.value:
+            self.status_label.Text = "Yeah you guessed my word"
+            exit(0)
+
+        if self.wordle_game.guesses == 0:
+            self.status_label.config(text='You were unable to guess my word.\n It was: ' + self.wordle_game.chosen_word)
 
 
 if __name__ == "__main__":
