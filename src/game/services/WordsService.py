66e95303c35af084d4a6dc4a630aba23474753ed
wfraga <walterfraga@yandex.com>
2024-02-07 17:08:06 -0500

    Added get_all_words method in WordsService

--- src/game/services/WordsService.py
+++ src/game/services/WordsService.py
@@ -15,4 +15,13 @@ class WordsService:
 
         return random_word[0]
 
+    def get_all_words(self):
+        all_words_response = requests.get(
+            url='https://random-word-api.herokuapp.com/all')
 
+        all_words = all_words_response.json()
+        returned_words = []
+        for word in all_words:
+            if len(word) == self.length:
+                returned_words.append(word)
+        return returned_words
